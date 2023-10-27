813509fb5eae78c15d2d90bc3ce1ed2378ddd952
Selim Haddioui <selim.haddioui@hotmail.com>
2023-10-27 16:39:13 +0200

    Update insert et create sql.
    Verification que l'apprenti & le tuteur existent dans la table d'utilisateur avant leur création dans la table apprenti.

--- src/main/resources/META-INF/sql/create_asta.sql
+++ src/main/resources/META-INF/sql/create_asta.sql
@@ -3,6 +3,6 @@ USE ASTA_DB;
 
 CREATE TABLE Utilisateur(Email VARCHAR(100) PRIMARY KEY, MotDePasse VARCHAR(100),Prenom VARCHAR(50), Nom VARCHAR(50), Tel VARCHAR(20));
 
-CREATE TABLE Tuteur(Email VARCHAR(100) PRIMARY KEY, MotDePasse VARCHAR(100),Prenom VARCHAR(50), Nom VARCHAR(50), Tel VARCHAR(20), Poste VARCHAR(100), Remarque VARCHAR(200), FOREIGN KEY(Email) REFERENCES Utilisateur(Email));
+CREATE TABLE Tuteur(Email VARCHAR(100) PRIMARY KEY, MotDePasse VARCHAR(100),Prenom VARCHAR(50), Nom VARCHAR(50), Tel VARCHAR(20), Poste VARCHAR(100), Remarque VARCHAR(200), FOREIGN KEY(Email, MotDePasse, Prenom, Nom, Tel) REFERENCES Utilisateur(Email, MotDePasse, Prenom, Nom, Tel));
 
-CREATE TABLE Apprenti(Email VARCHAR(100) PRIMARY KEY, MotDePasse VARCHAR(100),Prenom VARCHAR(50), Nom VARCHAR(50), Tel VARCHAR(20), Programme VARCHAR(50), Majeure VARCHAR(100), Annee VARCHAR(50), Archiver BOOLEAN, EmailTuteur VARCHAR(100), FOREIGN KEY(Email) REFERENCES Utilisateur(Email), FOREIGN KEY(EmailTuteur) REFERENCES Tuteur(Email));
+CREATE TABLE Apprenti(Email VARCHAR(100) PRIMARY KEY, MotDePasse VARCHAR(100),Prenom VARCHAR(50), Nom VARCHAR(50), Tel VARCHAR(20), Programme VARCHAR(50), Majeure VARCHAR(100), Annee VARCHAR(50), Archiver BOOLEAN, EmailTuteur VARCHAR(100), FOREIGN KEY(Email, MotDePasse, Prenom, Nom, Tel) REFERENCES Utilisateur(Email, MotDePasse, Prenom, Nom, Tel), FOREIGN KEY(EmailTuteur) REFERENCES Tuteur(Email));
--- src/main/resources/META-INF/sql/insert_asta.sql
+++ src/main/resources/META-INF/sql/insert_asta.sql
@@ -1,4 +1,4 @@
-INSERT INTO Utilisateur (Email, MotDePasse, Nom, Prenom, Tel) VALUES ('asta.tutor@efrei.net', 'tutor', 'Tutor1', 'tutor1', '0702070207');
+INSERT INTO Utilisateur (Email, MotDePasse, Nom, Prenom, Tel) VALUES ('tutor@efrei.net', 'tutor', 'ACHVAR', 'Didier', '0702070207');
 INSERT INTO Utilisateur (Email, MotDePasse, Nom, Prenom, Tel) VALUES ('student0@efrei.net', 'student', 'Student0-LN', 'student0-FN', '0423547569');
 INSERT INTO Utilisateur (Email, MotDePasse, Nom, Prenom, Tel) VALUES ('student1@efrei.net', 'student', 'Student1-LN', 'student1-FN', '0602060106');
 INSERT INTO Utilisateur (Email, MotDePasse, Nom, Prenom, Tel) VALUES ('student2@efrei.net', 'student', 'Student2-LN', 'student2-FN', '0602060106');
@@ -10,7 +10,7 @@ INSERT INTO Utilisateur (Email, MotDePasse, Nom, Prenom, Tel) VALUES ('student7@
 INSERT INTO Utilisateur (Email, MotDePasse, Nom, Prenom, Tel) VALUES ('student8@efrei.net', 'student', 'Student8-LN', 'student8-FN-Archived', '0702010205');
 INSERT INTO Utilisateur (Email, MotDePasse, Nom, Prenom, Tel) VALUES ('student9@efrei.net', 'student', 'Student9-LN', 'student9-FN-Archived','0402010506');
 
-INSERT INTO Tuteur (Email, MotDePasse, Nom, Prenom, Tel, Poste, Remarque) VALUES ('asta.tutor@efrei.net', 'tutor', 'Student1', 'Asta', '0702070207', 'Software Engineer', 'Une remarque');
+INSERT INTO Tuteur (Email, MotDePasse, Nom, Prenom, Tel, Poste, Remarque) VALUES ('tutor@efrei.net', 'tutor', 'ACHVAR', 'Didier', '0702070207', 'Software Engineer', 'Une remarque');
 
 INSERT INTO Apprenti (Email, MotDePasse, Nom, Prenom, Tel, Programme, Majeure, Annee, Archiver, EmailTuteur) VALUES ('student0@efrei.net', 'student', 'Student0-LN', 'student0-FN', '0423547569', 'LSI', 'Software Engineering', 2023, false, 'asta.tutor@efrei.net');
 INSERT INTO Apprenti (Email, MotDePasse, Nom, Prenom, Tel, Programme, Majeure, Annee, Archiver, EmailTuteur) VALUES ('student1@efrei.net', 'student', 'Student1-LN', 'student1-FN', '0602060106', 'LSI', 'Software Engineering', 2023, false, 'asta.tutor@efrei.net');
